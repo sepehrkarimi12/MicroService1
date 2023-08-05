58e9b60dac70d6045db218ac8e1cd3cd857b69bd
s.karimi <sepehr.k12@gmail.com>
2023-08-05 13:32:52 +0330

    used docer compose command to create container

--- Services/Catalog/Catalog.Api/Data/CatalogContextSeed.cs
+++ Services/Catalog/Catalog.Api/Data/CatalogContextSeed.cs
@@ -14,67 +14,37 @@ namespace Catalog.Api.Data
 
         private static IEnumerable<Product> GetSeedData()
         {
-            return new List<Product>
+            return new List<Product>()
             {
                 new Product
                 {
-                    Id = "1",
-                    Name = "Product1 Name",
-                    Summary = "Product1 Summary",
-                    Description = "Product1 Description",
-                    ImageFile = "Product1 ImageFile",
-                    Price = 240.00M,
-                    Category = "Product1 Category",
+                    Id="602d2149e773f2a3990b47f5",
+                    Name="Iphone X",
+                    Summary="This Phone is the company's biggest change to its flagship smartphone in years :)",
+                    Description="sjgjgsjkhgsdjhadgjahgsdjhasgdhjagdjhjd jgdsjgsakkhsagdjhasgdjhagsdjhgdhjagdsh hgajsgdhjags",
+                    ImageFile="Prod_1.png",
+                    Price= 950.00M,
+                    Category = "SmartPhone"
                 },
                 new Product
                 {
-                    Id = "2",
-                    Name = "Product2 Name",
-                    Summary = "Product2 Summary",
-                    Description = "Product2 Description",
-                    ImageFile = "Product2 ImageFile",
-                    Price = 200.00M,
-                    Category = "Product2 Category",
+                    Id="602d2149e773f2a3990b47f6",
+                    Name="Samsung A10",
+                    Summary="This Phone is the company's biggest change to its flagship smartphone in years :)",
+                    Description="dkhdlkjdhkjd djhd jdhkjdh khjdh kdjhdkjhdkjhopsiuwjisjik oidhj",
+                    ImageFile="Prod_2.png",
+                    Price= 840.00M,
+                    Category = "SmartPhone"
                 },
                 new Product
                 {
-                    Id = "3",
-                    Name = "Product3 Name",
-                    Summary = "Product3 Summary",
-                    Description = "Product3 Description",
-                    ImageFile = "Product3 ImageFile",
-                    Price = 300.00M,
-                    Category = "Product3 Category",
-                },
-                new Product
-                {
-                    Id = "4",
-                    Name = "Product4 Name",
-                    Summary = "Product4 Summary",
-                    Description = "Product4 Description",
-                    ImageFile = "Product4 ImageFile", 
-                    Price = 400.00M,
-                    Category = "Product4 Category",
-                },
-                new Product
-                {
-                    Id = "5",
-                    Name = "Product5 Name",
-                    Summary = "Product5 Summary",
-                    Description = "Product5 Description",
-                    ImageFile = "Product5 ImageFile",
-                    Price = 500.00M,
-                    Category = "Product5 Category",
-                },
-                new Product
-                {
-                    Id = "6",
-                    Name = "Product6 Name",
-                    Summary = "Product6 Summary",
-                    Description = "Product6 Description",
-                    ImageFile = "Product6 ImageFile",
-                    Price = 600.00M,
-                    Category = "Product6 Category",
+                    Id="602d2149e773f2a3990b47f7",
+                    Name="Xiaomi Poco X5 Pro",
+                    Summary="This Phone is the company's biggest change to its flagship smartphone in years :)",
+                    Description="dl;kjldjlkdjskjskllsihdjjhsghs hgs hsghs ghgshgshgshgss",
+                    ImageFile="Prod_3.png",
+                    Price= 900.00M,
+                    Category = "SmartPhone"
                 },
             };
         }
