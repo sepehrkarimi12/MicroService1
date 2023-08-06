d6f6e0e69a527af95c05633a980f9615db623782
s.karimi <sepehr.k12@gmail.com>
2023-08-06 14:02:03 +0330

    created Basket.Api project and installed Microsoft.Extensions.Caching.StackExchangeRedis package

---
+++ Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            
+        }
+
+        public ShoppingCart(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+    }
+}
---
+++ Services/Basket/Basket.Api/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//redis config
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost:6379";
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
