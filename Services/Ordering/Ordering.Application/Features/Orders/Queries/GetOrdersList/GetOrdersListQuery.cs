3b3dc3ed2161b47ff43d85599f474039fb3b0c83
s.karimi <sepehr.k12@gmail.com>
2023-08-16 15:17:25 +0330

    developed request and request hangler for implementing CQRS->mediatR

---
+++ Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>> //List<OrdersVm> : return type
+    {
+        public string UserName { get; set; }
+
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
